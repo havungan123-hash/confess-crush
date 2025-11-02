0e310e9ff57dc60c7c17db2f55e2d361debeebc5
havungan123-hash <havungan.123@gmail.com>
2025-11-02 18:38:37 +0700

    yeuyeuyeu

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    titleWeb: "NgHBachh",
+    introTitle: 'Gửi a🥲',
+    introDesc: `Ợ e bt là e hồ đồ xóa kb face vs a, e xinloii^^`,
+    btnIntro: '^^',
+    title: 'A cs thích e koq:))',
+    desc: 'Trả loi k hề ép buoc:v',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnNo: 'Không nha :(',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Áa e bt mầ:)',
+    messDesc: 'Tối nay 7h,e qa đón đi chơi nha.',
+    btnAccept: 'Okiee<3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
